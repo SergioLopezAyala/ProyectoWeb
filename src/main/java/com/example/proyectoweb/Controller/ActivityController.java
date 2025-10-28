72162855de82d6a9e581daa3fe3a1e71d0ee5f47
Urbinabina <125985060+Urbinabina@users.noreply.github.com>
2025-10-28 00:07:09 -0500

    backend drag n drop

--- src/main/java/com/example/proyectoweb/Controller/ActivityController.java
+++ src/main/java/com/example/proyectoweb/Controller/ActivityController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController @RequiredArgsConstructor
 @RequestMapping("/api/activities")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ActivityController {
 
     private final ActividadService service;
